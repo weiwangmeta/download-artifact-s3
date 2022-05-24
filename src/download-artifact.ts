969827874b61a84628e3c0a6fdf3bcf2a5bdc5ca
Eli Uriegas <eliuriegas@fb.com>
2022-05-24 09:43:19 -0700

    Revert "add runAttempt to s3Prefix"
    This reverts commit eb9aade5caadbcc25add108ec4bb0a2c7238e9b1.

--- src/download-artifact.ts
+++ src/download-artifact.ts
@@ -40,9 +40,7 @@ async function run(): Promise<void> {
     }
     core.debug(`Resolved path is ${resolvedPath}`)
     const s3 = new AWS.S3({region: region})
-    // TODO: This is a temporary bandaid, see https://github.com/actions/toolkit/pull/1027
-    const runAttempt = parseInt(process.env.GITHUB_RUN_ATTEMPT as string, 10)
-    const s3Prefix = `${github.context.repo.owner}/${github.context.repo.repo}/${github.context.runId}/${runAttempt}/${name}/`
+    const s3Prefix = `${github.context.repo.owner}/${github.context.repo.repo}/${github.context.runId}/${name}/`
     const s3Params = {
       Bucket: s3Bucket,
       Prefix: s3Prefix
